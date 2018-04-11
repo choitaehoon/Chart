e2cd482bb49782eea42a1c7fa70f33a84847a8f4
choitaehoon <xognsdl1230@naver.com>
2018-04-11 23:13:15 +0900

    jsoup작업 서비스단에 옮기기

--- src/main/java/chart/controller/ChartController.java
+++ src/main/java/chart/controller/ChartController.java
@@ -1,13 +1,7 @@
 package chart.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import chart.dto.User;
 import chart.mapper.UserMapper;
+import chart.service.Service;
 
 @Controller
 public class ChartController {
@@ -44,17 +39,8 @@ public class ChartController {
 	@RequestMapping("signUpAfter")
 	public String signUpAfter(User user, Model model) throws IOException
 	{
-		List<String> list = new ArrayList<>();
-		String listUrl = "";
-		Document document = Jsoup.connect(listUrl).get();
-		Elements tagList = document.select("p.title");
-		for (Element tag : tagList)
-		{
-			list.add(tag.text());
-			System.out.println(tag.text());
-		}
 		model.addAttribute("user", userMapper.select(user.getUserId()));
-		model.addAttribute("contents",list);
+		model.addAttribute("contents",Service.list());
 		return "signUpAfter";
 	}
 	
---
+++ src/main/java/chart/service/Service.java
@@ -0,0 +1,27 @@
+package chart.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Service 
+{
+	public static List<String> list() throws IOException
+	{
+		List<String> list = new ArrayList<>();
+		String listUrl = "https://music.bugs.co.kr/chart";
+		Document document = Jsoup.connect(listUrl).get();
+		Elements tagList = document.select("p.title");
+		for (Element tag : tagList)
+		{
+			list.add(tag.text());
+			System.out.println(tag.text());
+		}
+		return list;
+	}
+}
